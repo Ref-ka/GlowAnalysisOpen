c0e737af71c75c0fbfb54be96b1ac4abc58db47c
Ref_ka <228refka1337@gmail.com>
2025-04-25 20:58:51 +0500

    change calculate_mean_std for multiple dirs and change prepare_images for adjusting shift parameters

--- process/data_info.py
+++ process/data_info.py
@@ -2,37 +2,42 @@ import os
 import numpy as np
 from PIL import Image
 from torchvision.transforms import ToTensor
+from paths import TRAIN_DATA_DIR
 
 
-def calculate_mean_std(image_paths):
+def calculate_mean_std(image_dirs: list[str]):
     # Sums for each channel
     channel_sum = np.zeros(3)
     channel_sum_squared = np.zeros(3)
     num_pixels = 0
+    images_count = 0
 
-    for image_path in image_paths:
-        # Open image and transform it into tensor
-        image = Image.open(image_path).convert("RGB")
-        image_tensor = ToTensor()(image)  # Преобразуем в тензор (C, H, W)
+    for dir_name in image_dirs:
+        for directory_type in ["for_detector", "for_predictor"]:
+            directory_path = dir_name + f"\\images\\{directory_type}\\prepared"
+            directory = os.listdir(directory_path)
+            for image_path in directory:
+                # Open image and transform it into tensor
+                image = Image.open(directory_path + "\\" + image_path).convert("RGB")
+                image_tensor = ToTensor()(image)  # Преобразуем в тензор (C, H, W)
 
-        # Count sums and quad sums for each channel
-        channel_sum += image_tensor.view(3, -1).mean(dim=1).numpy()
-        channel_sum_squared += (image_tensor.view(3, -1) ** 2).mean(dim=1).numpy()
+                # Count sums and quad sums for each channel
+                channel_sum += image_tensor.view(3, -1).mean(dim=1).numpy()
+                channel_sum_squared += (image_tensor.view(3, -1) ** 2).mean(dim=1).numpy()
 
-        # Scaling overall amount of pixels
-        num_pixels += image_tensor.size(1) * image_tensor.size(2)
+                # Scaling overall amount of pixels
+                num_pixels += image_tensor.size(1) * image_tensor.size(2)
+
+        images_count += len(directory)
 
     # Calc mean and std
-    mean = channel_sum / len(image_paths)
-    std = np.sqrt(channel_sum_squared / len(image_paths) - mean ** 2)
+    mean = channel_sum / images_count
+    std = np.sqrt(channel_sum_squared / images_count - mean ** 2)
 
     return mean, std
 
 
 if __name__ == "__main__":
-    image_dir = "detector_train"
-    image_paths = [os.path.join(image_dir, img) for img in os.listdir(image_dir) if img.endswith(('.png', '.jpg', '.jpeg'))]
-
-    mean, std = calculate_mean_std(image_paths)
-    print(f"Mean: {mean}")
-    print(f"Std: {std}")
+    dir_list = ["14.04.2025"]
+    dir_list = list(map(lambda dir_name: TRAIN_DATA_DIR + "\\" + dir_name, dir_list))
+    print(calculate_mean_std(dir_list))
--- process/prepariring_for_labeling.py
+++ process/prepariring_for_labeling.py
@@ -1,34 +1,38 @@
 from process.image_preprocessing import preprocess_images
 from process.video_to_images import cut_videos
 from process.image_resizing import crop_images
+from process.data_info import calculate_mean_std
 
 from paths import TRAIN_DATA_DIR
 
 
-def prepare_for_detector(dir_list: list[str]):
-    for directory in dir_list:
-        data_dir = TRAIN_DATA_DIR + "\\" + directory
+def prepare_images(dir_list: list[str], image_size: int, shift_list: list[list[list]]):
+    dir_list = list(map(lambda dir_name: TRAIN_DATA_DIR + "\\" + dir_name, dir_list))
+    for i, directory in enumerate(dir_list):
 
         # preprocess images for detector
-        cut_videos(data_dir + "\\videos",
-                   data_dir + "\\images\\for_detector\\unprepared",
+        cut_videos(directory + "\\videos",
+                   directory + "\\images\\for_detector\\unprepared",
                    40)
-        preprocess_images(data_dir + "\\images\\for_detector\\unprepared",
-                          data_dir + "\\images\\for_detector\\preprocessed_1080")
-        crop_images(data_dir + "\\images\\for_detector\\preprocessed_1080",
-                    300,
+        preprocess_images(directory + "\\images\\for_detector\\unprepared",
+                          directory + "\\images\\for_detector\\preprocessed_1080")
+        crop_images(directory + "\\images\\for_detector\\preprocessed_1080",
+                    image_size,
                     "c",
-                    data_dir + "\\images\\for_detector\\prepared",
-                    [-150, -150])
+                    directory + "\\images\\for_detector\\prepared",
+                    shift_list[i][0])
 
         # preprocess images for predictor
-        preprocess_images(data_dir + "\\images\\for_predictor\\unprepared",
-                          data_dir + "\\images\\for_predictor\\preprocessed_1080")
-        crop_images(data_dir + "\\images\\for_predictor\\preprocessed_1080",
-                    300,
+        preprocess_images(directory + "\\images\\for_predictor\\unprepared",
+                          directory + "\\images\\for_predictor\\preprocessed_1080")
+        crop_images(directory + "\\images\\for_predictor\\preprocessed_1080",
+                    image_size,
                     "c",
-                    data_dir + "\\images\\for_predictor\\prepared")
+                    directory + "\\images\\for_predictor\\prepared",
+                    shift_list[i][1])
+
+    print(calculate_mean_std(dir_list))
 
 
 if __name__ == "__main__":
-    prepare_for_detector(["14.04.2025"])
+    prepare_images(["17.01.2025"], 300, [[[80, -140], [0, -45]]])
